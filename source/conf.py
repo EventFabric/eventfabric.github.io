9aa6cf2ed78dd8a0951817f913ba35d5f49a4e80
Mariano Guerra <mariano@marianoguerra.org>
2013-09-20 15:06:19 +0000

    add google analytics extension

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,9 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinxcontrib.googleanalytics']
+googleanalytics_id = 'UA-44215851-1'
+googleanalytics_domain = 'eventfabric.github.io'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
